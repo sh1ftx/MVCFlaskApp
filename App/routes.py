f63f8c7abbfaceb5591499f27744506c4fff88b9
vinycius <vinyciusalves155@gmail.com>
2025-04-04 17:27:44 -0300

    apenas adicoes de commits no codigo

--- App/routes.py
+++ App/routes.py
@@ -2,18 +2,19 @@ from flask import request, jsonify, render_template
 # from app.models import database
 #presumindo que o gleisun vai fazer algo pra retornar o banco
 
+#db = funcao de conexao com o database
 
 #Função para adicionar o usuario ao banco de dados
 @app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
     if request.method == "POST":
         dados = request.json
-        
         cursor = db.cursor()
+
+        #Inserção do usuario ao database
         cursor.execute(
-            "INSERT INTO usuarios (nome, idade, email, telefone, cidade, data_nascimento) VALUES (%s, %s, %s, %s, %s, %s)",
-            (dados.get("nome"),
-             dados.get("idade"), 
+            "INSERT INTO usuarios (nome, email, telefone, cidade, data_nascimento) VALUES (%s, %s, %s, %s, %s)",
+            (dados.get("nome"), 
              dados.get("email"),
              dados.get("telefone"),
              dados.get("cidade"),
