7c81da2edcec44bf61f0ee538b11c370a8db92ad
vinycius <vinyciusalves155@gmail.com>
2025-04-09 12:58:40 -0300

    ajustes de nomenclatura e fluxo do site

--- App/static/js/user_register.js
+++ App/static/js/user_register.js
@@ -2,14 +2,14 @@ document.getElementById("cadastroForm").addEventListener("submit", async (e) => 
     e.preventDefault();
 
     const dados = {
-        nome: document.getElementById("nome").value,
-        email: document.getElementById("email").value,
-        telefone: document.getElementById("telefone").value,
-        cidade: document.getElementById("cidade").value,
-        data_nascimento: document.getElementById("data_nascimento").value
+        "nome": document.getElementById("nome").value,
+        "email": document.getElementById("email").value,
+        "telefone": document.getElementById("telefone").value,
+        "cidade": document.getElementById("cidade").value,
+        "data_nascimento": document.getElementById("data_nascimento").value
     };
 
-    const response = await fetch("/register", {
+    const response = await fetch("/user_register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(dados)
@@ -19,6 +19,7 @@ document.getElementById("cadastroForm").addEventListener("submit", async (e) => 
 
     if (response.ok) {
         alert(result.message);
+        window.location.href = "/user_list";
     } else {
         alert(result.error || "Erro ao cadastrar!");
     }
